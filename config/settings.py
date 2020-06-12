4f806f9a65b0f63dddc4cc38fd8bae23bb7df7d3
lindsayAhn <ek991128@naver.com>
2020-06-12 20:12:10 +0900

    COVID-19

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2b_+$18i7byku_sx7r5ju)7+eus7*k#vm#m-=kq%1y4*s^es35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'lindsayan.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
