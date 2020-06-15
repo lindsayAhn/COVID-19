6e28cc315c3cdabc15226cd7cb787e763396c3a9
lindsayAhn <59251566+lindsayAhn@users.noreply.github.com>
2020-06-15 21:59:22 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2b_+$18i7byku_sx7r5ju)7+eus7*k#vm#m-=kq%1y4*s^es35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'lindsayan.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
