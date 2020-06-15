af0f548abfd1f44c9d00392406c0f53726c422c6
lindsayAhn <59251566+lindsayAhn@users.noreply.github.com>
2020-06-15 21:59:04 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from chart import views                                     # !!!
+from chart import views
 
 urlpatterns = [
     path('covid/',
-         views.covid19, name='covid19'),  # !!!
+         views.covid19, name='covid19'),
+    path('world-population/',
+         views.world_population, name='world_population'),
+    path('titanic/',
+         views.titanic, name='titanic'),
     path('admin/', admin.site.urls),
 ]
